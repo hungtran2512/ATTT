62b86759446feba380d43a55c60cb90af665e32a
Ziiiqaztyu <chauphuduy2333@gmail.com>
2024-08-29 16:11:43 +0700

    upcode

---
+++ IOT - code cho đầu ghi.ino
@@ -0,0 +1,50 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(2, 3); // RX nối với TX (lora), TX - RX (lora)
+
+#define SCALE_RX 10 // Chân RX nối với cân (nếu cân sử dụng một chân khác)
+#define SCALE_TX 11 // Chân TX nối với cân (nếu cân sử dụng một chân khác)
+
+SoftwareSerial scaleSerial(SCALE_RX, SCALE_TX); // Giao tiếp với cân
+
+void setup()
+{
+  Serial.begin(9600); // Khởi tạo giao tiếp với máy tính
+  mySerial.begin(9600); // Khởi tạo giao tiếp với module LoRa
+  scaleSerial.begin(9600); // Khởi tạo giao tiếp với cân
+}
+
+void loop()
+{
+/*
+    Đọc dữ liệu từ cân
+    Gửi dữ liệu đi bằng UART LoRa
+  */
+    if (scaleSerial.available() > 0)
+    {
+    String inputWeight = "";
+    inputWeight = scaleSerial.readStringUntil('\n'); // Đọc dữ liệu từ cân cho đến khi gặp ký tự xuống dòng
+    if (inputWeight != "")
+    {
+      inputWeight.trim(); // Loại bỏ khoảng trắng đầu và cuối
+      Serial.println(inputWeight); // In dữ liệu ra màn hình Serial Monitor
+      mySerial.println(inputWeight); // Gửi dữ liệu qua module LoRa
+    }
+    }
+
+    /*
+    Nhận dữ liệu từ LoRa (UART: RX, TX)
+    In ra màn hình Serial
+  */
+    if (mySerial.available() > 0)
+    {
+    String inputReceive = mySerial.readStringUntil('\n'); // Đọc dữ liệu từ LoRa
+
+    if (inputReceive != "")
+    {
+      inputReceive.trim(); // Loại bỏ khoảng trắng đầu và cuối
+      Serial.println(inputReceive); // In dữ liệu ra màn hình Serial Monitor
+    }
+    }
+  delay(20); // Đợi 20 mili giây
+}
