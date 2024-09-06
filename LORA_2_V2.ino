021654e4d7e168a01db22deafc2bd1e47b75bfbc
Cornnnie <159974181+Cornnnie@users.noreply.github.com>
2024-09-06 10:59:41 +0700

    Update LORA_2_V2.ino
    Chương trình này thực hiện hai chức năng chính: gửi dữ liệu từ máy tính đến module Lora và điều khiển đèn LED dựa trên dữ liệu nhận được từ module Lora.

--- LORA_2_V2.ino
+++ LORA_2_V2.ino
@@ -1,15 +1,21 @@
 #include <SoftwareSerial.h>
-SoftwareSerial mySerial(2, 3); //RX nối với TX(lora), TX - RX(lora)
+// Thiết lập giao tiếp UART với module Lora (RX chân 2, TX chân 3)
+SoftwareSerial mySerial(2, 3); 
 
+// Định nghĩa chân M0, M1 để điều chỉnh chế độ của Lora (không sử dụng trong đoạn code này)
 #define M0 8
 #define M1 9
-#define LED 13
-// M0, M1 đấu với gnd: Normal mode
+
+// Định nghĩa chân điều khiển đèn LED
+#define LED 13 
 
 void setup()
 {
+  // Khởi tạo Serial Monitor (giao tiếp với máy tính) và Lora với tốc độ 9600 baud
   Serial.begin(9600);
   mySerial.begin(9600);
+  
+  // Cấu hình chân LED làm đầu ra, tắt đèn LED khi khởi động
   pinMode(LED, OUTPUT);
   digitalWrite(LED, LOW);
 }
@@ -17,38 +23,35 @@ void setup()
 void loop()
 {
   /*
-     Đọc dữ liệu  từ cổng Serial monitor
-     Gửi dữ liệu đi bằng uart Lora
+     Đọc dữ liệu từ máy tính thông qua Serial Monitor
+     Gửi dữ liệu nhận được từ máy tính qua Lora
   */
-  if (Serial.available() > 0)
+  if (Serial.available() > 0) // Kiểm tra nếu có dữ liệu nhập từ máy tính
   {
-    String inputSend = "";
-    inputSend = Serial.readStringUntil('\n');
-    mySerial.println(inputSend);
+    String inputSend = ""; 
+    inputSend = Serial.readStringUntil('\n'); // Đọc dữ liệu nhập từ Serial Monitor
+    mySerial.println(inputSend); // Gửi dữ liệu qua Lora
   }
 
   /*
-     nhận dữ liệu từ lora(uart: rx, tx)
-     in ra màn hình Serial
+     Nhận dữ liệu từ module Lora qua giao tiếp UART
+     Hiển thị dữ liệu nhận được trên Serial Monitor
+     Điều khiển LED nếu nhận được lệnh "on" hoặc "off"
   */
-  if (mySerial.available() > 1)
+  if (mySerial.available() > 1) // Kiểm tra nếu có dữ liệu từ Lora
   {
-    String inputReceive = mySerial.readStringUntil('\n');
+    String inputReceive = mySerial.readStringUntil('\n'); // Đọc dữ liệu từ Lora
 
-    if (inputReceive != "")
+    if (inputReceive != "") // Nếu dữ liệu nhận không rỗng
     {
-      inputReceive.trim();
-      Serial.println(inputReceive);
-      if (inputReceive == "on")
+      inputReceive.trim(); // Xóa khoảng trắng thừa nếu có
+      Serial.println(inputReceive); // Hiển thị dữ liệu trên Serial Monitor
+      
+      // Kiểm tra dữ liệu và điều khiển LED
+      if (inputReceive == "on") 
       {
-        digitalWrite(LED, HIGH);
+        digitalWrite(LED, HIGH); // Bật LED nếu nhận lệnh "on"
       }
       else if (inputReceive == "off")
       {
-        digitalWrite(LED, LOW);
-      }
-      inputReceive = "";
-    }
-  }
-  delay(20);
-}
+        digitalWrite(LED, LOW); // Tắt LED
